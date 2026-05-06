7c309db16acf45b8d5087755faf9b56cf1d828fb
kimsam <92019444+KimSaem@users.noreply.github.com>
2026-05-06 21:53:16 +1200

    Update stock.js

--- functions/api/stock.js
+++ functions/api/stock.js
@@ -3,12 +3,21 @@ import { getItems } from "../services/itemService.js";
 
 export default {
   async fetch(req, env) {
-    const db = createDB(env);
+    try {
+      const db = createDB(env);
 
-    const items = await getItems(db);
+      const items = await getItems(db);
 
-    return new Response(JSON.stringify(items), {
-      headers: { "Content-Type": "application/json" }
-    });
+      return new Response(JSON.stringify(items), {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+    } catch (e) {
+      return new Response(
+        JSON.stringify({ error: e.message }),
+        { status: 500 }
+      );
+    }
   }
 };
