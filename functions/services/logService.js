36314e2e97c775737070bc2c29903524502ded0a
kimsam <92019444+KimSaem@users.noreply.github.com>
2026-05-06 21:52:37 +1200

    Update logService.js

--- functions/services/logService.js
+++ functions/services/logService.js
@@ -1,15 +1,19 @@
 export async function insertLog(db, log) {
   const { item_id, from, to, qty } = log;
 
-  await db.run(
-    `INSERT INTO stock_logs (item_id, from_location, to_location, qty)
-     VALUES (?, ?, ?, ?)`,
-    [item_id, from, to, qty]
-  );
+  return await db
+    .prepare(
+      `INSERT INTO stock_logs (item_id, from_location, to_location, qty)
+       VALUES (?, ?, ?, ?)`
+    )
+    .bind(item_id, from, to, qty)
+    .run();
 }
 
 export async function getLogs(db) {
-  return await db.all(
-    `SELECT * FROM stock_logs ORDER BY created_at DESC`
-  );
+  const res = await db
+    .prepare("SELECT * FROM stock_logs ORDER BY created_at DESC")
+    .all();
+
+  return res.results || [];
 }
