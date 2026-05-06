8e7a6b69effa23bb6afa2b0842bbe0f4b84f0278
kimsam <92019444+KimSaem@users.noreply.github.com>
2026-05-06 21:48:00 +1200

    Create log.js

---
+++ functions/api/log.js
@@ -0,0 +1,14 @@
+import { createDB } from "../db/client.js";
+import { getLogs } from "../services/logService.js";
+
+export default {
+  async fetch(req, env) {
+    const db = createDB(env);
+
+    const logs = await getLogs(db);
+
+    return new Response(JSON.stringify(logs), {
+      headers: { "Content-Type": "application/json" }
+    });
+  }
+};
