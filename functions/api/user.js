22c9a2b13c40b80b449dcea25c113d9abf7bd5e6
kimsam <92019444+KimSaem@users.noreply.github.com>
2026-05-06 22:28:46 +1200

    Update user.js

--- functions/api/user.js
+++ functions/api/user.js
@@ -2,14 +2,39 @@ import { createDB } from "../db/client.js";
 
 export default {
   async fetch(req, env) {
-    const db = createDB(env);
+    try {
+      if (!env.DB) {
+        throw new Error("DB_NOT_BOUND");
+      }
 
-    const users = await db
-      .prepare("SELECT * FROM users")
-      .all();
+      const db = createDB(env);
 
-    return new Response(
-      JSON.stringify(users.results ?? [])
-    );
+      const res = await db
+        .prepare("SELECT * FROM users ORDER BY id DESC")
+        .all();
+
+      const users = res?.results ?? [];
+
+      return new Response(
+        JSON.stringify({
+          success: true,
+          data: users
+        }),
+        {
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }
+      );
+
+    } catch (e) {
+      return new Response(
+        JSON.stringify({
+          success: false,
+          error: e.message
+        }),
+        { status: 500 }
+      );
+    }
   }
 };
