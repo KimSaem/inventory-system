9f728ccc9e0b83896a2119f3ce5cd0b0343c07c4
kimsam <92019444+KimSaem@users.noreply.github.com>
2026-05-06 22:42:51 +1200

    Update stock.js

--- functions/api/stock.js
+++ functions/api/stock.js
@@ -1,41 +1,10 @@
-import { createDB } from "../db/client.js";
-
 export default {
   async fetch(req, env) {
-    try {
-      if (!env.DB) {
-        throw new Error("DB_NOT_BOUND");
-      }
-
-      const db = createDB(env);
-
-      const res = await db
-        .prepare("SELECT * FROM stock ORDER BY id DESC")
-        .all();
-
-      const stock = res?.results ?? [];
-
-      return new Response(
-        JSON.stringify({
-          success: true,
-          data: stock
-        }),
-        {
-          headers: {
-            "Content-Type": "application/json",
-            "Cache-Control": "no-store"
-          }
-        }
-      );
-
-    } catch (e) {
-      return new Response(
-        JSON.stringify({
-          success: false,
-          error: e.message
-        }),
-        { status: 500 }
-      );
-    }
+    return new Response(
+      JSON.stringify({
+        db: !!env.DB,
+        keys: Object.keys(env || {})
+      })
+    );
   }
 };
