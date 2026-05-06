4b3be00c801118ec2ea94e1ee58b9b26406a5eb1
kimsam <92019444+KimSaem@users.noreply.github.com>
2026-05-06 22:14:19 +1200

    Update client.js

--- functions/db/client.js
+++ functions/db/client.js
@@ -1,17 +1,3 @@
 export function createDB(env) {
-  return {
-    db: env.DB,
-
-    async all(query, params = []) {
-      return await this.db.prepare(query).bind(...params).all();
-    },
-
-    async get(query, params = []) {
-      return await this.db.prepare(query).bind(...params).first();
-    },
-
-    async run(query, params = []) {
-      return await this.db.prepare(query).bind(...params).run();
-    }
-  };
+  return env.DB;
 }
