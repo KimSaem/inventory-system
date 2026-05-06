1366cb7181f28bc80c7be999fbd0e2adc5d2668a
kimsam <92019444+KimSaem@users.noreply.github.com>
2026-05-06 21:52:23 +1200

    Update itemService.js

--- functions/services/itemService.js
+++ functions/services/itemService.js
@@ -1,27 +1,21 @@
-export async function createItem(db, data) {
-  return await db.run(
-    `INSERT INTO items (name, home_qty, store_qty)
-     VALUES (?, ?, ?)`,
-    [data.name, data.home_qty || 0, data.store_qty || 0]
-  );
+export async function getItems(db) {
+  const res = await db.prepare("SELECT * FROM items").all();
+  return res.results || [];
 }
 
-export async function updateItem(db, id, data) {
-  return await db.run(
-    `UPDATE items
-     SET name = ?, home_qty = ?, store_qty = ?
-     WHERE id = ?`,
-    [data.name, data.home_qty, data.store_qty, id]
-  );
+export async function createItem(db, data) {
+  return await db
+    .prepare(
+      `INSERT INTO items (name, home_qty, store_qty)
+       VALUES (?, ?, ?)`
+    )
+    .bind(data.name, data.home_qty || 0, data.store_qty || 0)
+    .run();
 }
 
 export async function deleteItem(db, id) {
-  return await db.run(
-    `DELETE FROM items WHERE id = ?`,
-    [id]
-  );
-}
-
-export async function getItems(db) {
-  return await db.all(`SELECT * FROM items`);
+  return await db
+    .prepare("DELETE FROM items WHERE id = ?")
+    .bind(id)
+    .run();
 }
