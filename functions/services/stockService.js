8181627062c69f5d058d06bcf9a27aff41ac94d4
kimsam <92019444+KimSaem@users.noreply.github.com>
2026-05-06 22:15:03 +1200

    Create stockService.js

---
+++ functions/services/stockService.js
@@ -0,0 +1,14 @@
+export async function getStock(db) {
+  const res = await db
+    .prepare("SELECT * FROM stock ORDER BY id DESC")
+    .all();
+
+  return res.results ?? [];
+}
+
+export async function updateStock(db, id, qty) {
+  return await db
+    .prepare("UPDATE stock SET quantity = ? WHERE id = ?")
+    .bind(qty, id)
+    .run();
+}
