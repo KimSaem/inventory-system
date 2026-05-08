6015735c496585d325350b023fa94d987399a894
kimsam <92019444+KimSaem@users.noreply.github.com>
2026-05-08 22:35:30 +1200

    Update auto-order.js

--- functions/api/auto-order.js
+++ functions/api/auto-order.js
@@ -1,15 +1,51 @@
-let AUTO_ORDERS = [];
+import { aiOrderCalculator }
+from "../lib/ai-order.js";
 
-export async function autoOrder(req) {
+export async function onRequestPost(context){
 
-  const body = await req.json();
+  try{
 
-  AUTO_ORDERS.push({
-    time: Date.now(),
-    orders: body.orders
-  });
+    const db = context.env.DB;
 
-  return new Response(JSON.stringify({
-    success: true
-  }));
+    const body =
+      await context.request.json();
+
+    const stock_id =
+      body.stock_id;
+
+    const item =
+      await db.prepare(`
+        SELECT *
+        FROM stock
+        WHERE id=?
+      `)
+      .bind(stock_id)
+      .first();
+
+    if(!item){
+
+      return Response.json({
+        success:false,
+        error:"ITEM NOT FOUND"
+      });
+    }
+
+    const ai_order =
+      aiOrderCalculator(item);
+
+    return Response.json({
+      success:true,
+      item:{
+        ...item,
+        ai_order
+      }
+    });
+
+  }catch(err){
+
+    return Response.json({
+      success:false,
+      error:err.message
+    });
+  }
 }
